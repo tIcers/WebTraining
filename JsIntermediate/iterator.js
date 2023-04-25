f9b65fd7c877b377fd2c1176ff64f5a858008218
atsuki <csatsfapsfu2930@gmail.com>
2023-04-25 12:20:13 -0700

    functino as param

---
+++ JsIntermediate/iterator.js
@@ -0,0 +1,36 @@
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+	for(let i = 1; i <= 1000000; i++) {
+	  if ( (2 + 2) != 4) {
+		console.log('Something has gone very wrong :( ');
+	  }
+	}
+  };
+  
+  // Write your code below
+  
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes 
+
+isTwoPlusTwo()
+
+console.log(isTwoPlusTwo.name)
+
+
+// Functions as Parameters
+
+const addTwo = num => {
+	return num + 2;
+  }
+  
+  const checkConsistentOutput = (func, val) => {
+	let checkA = val + 2
+	let checkB = func(val)
+	if(checkA === checkB){
+		return checkB 
+	}else{
+		return 'inconsistent results'
+	}
+  
+  }
+  
+  console.log();
+console.log(checkConsistentOutput(addTwo, 2))
