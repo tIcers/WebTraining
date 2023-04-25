1fffddb0a9fd511ce309234fef75d8aeb9838d53
atsuki <csatsfapsfu2930@gmail.com>
2023-04-25 11:28:36 -0700

    pracitced getter and setter in js

--- JsIntermediate/mealMaker.js
+++ JsIntermediate/mealMaker.js
@@ -1,3 +1,31 @@
 const menu = {
-	_menu = ''
-}
+	_menu: '',
+	_price: '',
+	
+	set mealChecker(mealToCheck) {
+	  if (typeof mealToCheck === 'string') {
+		this._menu = mealToCheck;
+	  }
+	},
+	
+	set priceChecker(priceToCheck) {
+	  if (typeof priceToCheck === 'number') {
+		this._price = priceToCheck;
+	  }
+	},
+	
+	get todaysSpecial() {
+	  if (this._menu && this._price) {
+		return `Today's Special is ${this._menu} for \$${this._price}` 
+	  } else {
+		return 'Meal or price was not set correctly!'
+	  }	
+	}
+  };
+  
+  menu.mealChecker = 'protein';
+  console.log(menu._menu);
+  menu.priceChecker = 10.99;
+  console.log(menu._price)
+  console.log(menu.todaysSpecial);
+  
