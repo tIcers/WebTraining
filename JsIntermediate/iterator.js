af0cf5512714e0963772146374a24e33ce9f25eb
atsuki <csatsfapsfu2930@gmail.com>
2023-04-25 13:04:01 -0700

    practiced map

--- JsIntermediate/iterator.js
+++ JsIntermediate/iterator.js
@@ -1,36 +1,23 @@
-const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
-	for(let i = 1; i <= 1000000; i++) {
-	  if ( (2 + 2) != 4) {
-		console.log('Something has gone very wrong :( ');
-	  }
-	}
-  };
-  
-  // Write your code below
-  
-const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes 
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-isTwoPlusTwo()
-
-console.log(isTwoPlusTwo.name)
+// Iterate over fruits below
+fruits.forEach(function(items) {
+	console.log('I want to eat a ' + items)
+});
 
 
-// Functions as Parameters
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-const addTwo = num => {
-	return num + 2;
-  }
-  
-  const checkConsistentOutput = (func, val) => {
-	let checkA = val + 2
-	let checkB = func(val)
-	if(checkA === checkB){
-		return checkB 
-	}else{
-		return 'inconsistent results'
-	}
-  
-  }
-  
-  console.log();
-console.log(checkConsistentOutput(addTwo, 2))
+// Create the secretMessage array below
+const secretMessage = animals.map(animals => {
+	return animals.charAt(0)
+})
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(bigNumbers =>{
+	return bigNumbers / 100
+})
