a5be1f8d718b415d487f2c14510f195fe466d765
atsuki <csatsfapsfu2930@gmail.com>
2023-04-26 15:04:35 -0700

    added foreach

--- JsIntermediate/codingChallenge.js
+++ JsIntermediate/codingChallenge.js
@@ -26,17 +26,30 @@ greetAliens(aliens);
 
 // Write your code here:
 
-
-
-
-
-
-
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+const convertToBaby = animals.map(function(elements){
+	return 'baby ' + elements
+})
 
 // When you're ready to test your code, uncomment the below and run:
 
-const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
 
-console.log(convertToBaby(animals)) 
+console.log(convertToBaby) 
 
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
+
+
+const declineEverything = arr => {
+  arr.forEach(politelyDecline)
+}
+
+const acceptEverything = (arr) => {
+	arr.forEach( e => {
+	console.log(`Ok, please, I will eat some  + ${e}`)
+	})
+}
 
