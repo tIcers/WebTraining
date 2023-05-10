bf3fcaf11a2bd70776f9f87dd700e3c4e7f276e8
atsuki <csatsfapsfu2930@gmail.com>
2023-05-10 01:24:09 -0700

    traversing dom and stuff

--- websiteJS/DOM/main.js
+++ websiteJS/DOM/main.js
@@ -9,3 +9,18 @@ document.getElementById("fourth").innerHTML = "Professor Snape";
 document.getElementsByClassName("slytherin")[0].innerHTML = "Salazar Slytherin";
 
 document.getElementsByTagName("li")[0].innerHTML = "Dobby";
+
+// Traversing the DOM
+const first = document.body.children[0];
+
+first.innerHTML = "BROWN BEARS ARE AWESOME!";
+
+first.parentNode.style.backgroundColor = "beige";
+
+let newAttraction = document.createElement("li");
+
+newAttraction.id = "vespa";
+
+newAttraction.innerHTML = "Rent a Vespa";
+
+document.getElementById(":italy-attractions").appendChild(newAttraction);
