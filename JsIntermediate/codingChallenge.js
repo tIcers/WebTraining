1c9cde8426e9ed4fdd4c25fe4d979cbafc5fbea4
atsuki <csatsfapsfu2930@gmail.com>
2023-05-09 22:42:36 -0700

    added coding challenging

--- JsIntermediate/codingChallenge.js
+++ JsIntermediate/codingChallenge.js
@@ -1,120 +1,128 @@
-function reverseArray(arr){
+function reverseArray(arr) {
   let reverseArr = [];
-  for(let i = arr.length - 1; i >= 0; i --){
-    reverseArr.push(arr[i])
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reverseArr.push(arr[i]);
   }
   return reverseArr;
 }
-const sentence = ['sense.','make', 'all', 'will', 'This'];
-const reversedSentence = reverseArray(sentence)
-console.log(reversedSentence) 
+const sentence = ["sense.", "make", "all", "will", "This"];
+const reversedSentence = reverseArray(sentence);
+console.log(reversedSentence);
 // Should print ['This', 'will', 'all', 'make', 'sense.'];
 
 // Write your code here:
 
 const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
 
-function greetAliens(aliens){
-for(let i = 0; i < aliens.length; i ++){
-	console.log("Oh powerful " + aliens[i] + ", " + "we humans offer our unconditional surrender!")
-}
-// When you're ready to test your code, uncomment the below and run:
-
-
+function greetAliens(aliens) {
+  for (let i = 0; i < aliens.length; i++) {
+    console.log(
+      "Oh powerful " +
+        aliens[i] +
+        ", " +
+        "we humans offer our unconditional surrender!"
+    );
+  }
+  // When you're ready to test your code, uncomment the below and run:
 }
 greetAliens(aliens);
 
 // Write your code here:
 
-const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
-const convertToBaby = animals.map(function(elements){
-	return 'baby ' + elements
-})
+const animals = ["panda", "turtle", "giraffe", "hippo", "sloth", "human"];
+const convertToBaby = animals.map(function (elements) {
+  return "baby " + elements;
+});
 
 // When you're ready to test your code, uncomment the below and run:
 
+console.log(convertToBaby);
 
-console.log(convertToBaby) 
-
-const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+const veggies = ["broccoli", "spinach", "cauliflower", "broccoflower"];
 
 const politelyDecline = (veg) => {
-      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
-}
+  console.log("No " + veg + " please. I will have pizza with extra cheese.");
+};
 
-
-const declineEverything = arr => {
-  arr.forEach(politelyDecline)
-}
+const declineEverything = (arr) => {
+  arr.forEach(politelyDecline);
+};
 
 const acceptEverything = (arr) => {
-	arr.forEach( e => {
-	console.log(`Ok, please, I will eat some  + ${e}`)
-	})
-}
+  arr.forEach((e) => {
+    console.log(`Ok, please, I will eat some  + ${e}`);
+  });
+};
 
-const numbers = [2, 7, 9, 171, 52, 33, 14]
+const numbers = [2, 7, 9, 171, 52, 33, 14];
 
-const toSquare = num => num * num
+const toSquare = (num) => num * num;
 
 // Write your code here:
-const squareNums = arr => arr.map(toSquare) 
+const squareNums = (arr) => arr.map(toSquare);
 
+const greetings = ["hello", "hi", "heya", "oi", "hey", "yo"];
 
+const shoutGreetings = (arr) =>
+  arr.map(function (elements) {
+    return elements.toUpperCase() + "!";
+  });
 
-
-const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
-
-const shoutGreetings =arr => arr.map(function(elements){
-	return elements.toUpperCase() + '!'
-})
-
-console.log(shoutGreetings(greetings))
+console.log(shoutGreetings(greetings));
 // Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
 
-
 // Write your code here:
 
-function sortYears(arr){
-	arr.sort(function(a, b) {
-    return b - a
-  })
-  return arr
+function sortYears(arr) {
+  arr.sort(function (a, b) {
+    return b - a;
+  });
+  return arr;
 }
-
-
-
 
 // Feel free to uncomment the below code to test your function:
 
-const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922];
 
-console.log(sortYears(years))
+console.log(sortYears(years));
 // Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
 
-function justCoolStuff(arr1, arr2){
-	return arr1.filter(function(element){
-	  return arr2.includes(element);
-	});
-  }
-  
-  // Test the function
-  const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
-  const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
-  
-  console.log(justCoolStuff(myStuff, coolStuff));
-  // Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+function justCoolStuff(arr1, arr2) {
+  return arr1.filter(function (element) {
+    return arr2.includes(element);
+  });
+}
+
+// Test the function
+const coolStuff = [
+  "gameboys",
+  "skateboards",
+  "backwards hats",
+  "fruit-by-the-foot",
+  "pogs",
+  "my room",
+  "temporary tattoos",
+];
+const myStuff = [
+  "rules",
+  "fruit-by-the-foot",
+  "wedgies",
+  "sweaters",
+  "skateboards",
+  "family-night",
+  "my room",
+  "braces",
+  "the information superhighway",
+];
+
+console.log(justCoolStuff(myStuff, coolStuff));
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
 
 // Write your code here:
 
-function isTheDinnerVegan(meal){
-	return meal.every(food => food.source ==='plant')  
+function isTheDinnerVegan(meal) {
+  return meal.every((food) => food.source === "plant");
 }
-
-
-
-
-
 
 // Feel free to comment out the code below to test your function
 /*
@@ -127,23 +135,23 @@ console.log(isTheDinnerVegan(dinner))
 // Should print false
 */
 
-const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+const speciesArray = [
+  { speciesName: "shark", numTeeth: 50 },
+  { speciesName: "dog", numTeeth: 42 },
+  { speciesName: "alligator", numTeeth: 80 },
+  { speciesName: "human", numTeeth: 32 },
+];
 
 // Write your code here:
-function sortSpeciesByTeeth(speciesArray){
-	return speciesArray.sort(function(a, b){
-		return a.numTeeth - b.numTeeth
-	})
+function sortSpeciesByTeeth(speciesArray) {
+  return speciesArray.sort(function (a, b) {
+    return a.numTeeth - b.numTeeth;
+  });
 }
-
-
-
-
-
 
 // Feel free to comment out the code below when you're ready to test your function!
 
-console.log(sortSpeciesByTeeth(speciesArray))
+console.log(sortSpeciesByTeeth(speciesArray));
 
 // Should print:
 // [ { speciesName: 'human', numTeeth: 32 },
@@ -151,52 +159,59 @@ console.log(sortSpeciesByTeeth(speciesArray))
 //   { speciesName: 'shark', numTeeth: 50 },
 //   { speciesName: 'alligator', numTeeth: 80 } ]
 
-function findMyKeys(array){
-	let i = 0;
-	while(i < array.length){
-		if(array[i] === 'keys'){
-			return i;
-		}
-		i++;
-	}
-	return -1
-}	
-const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+function findMyKeys(array) {
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] === "keys") {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+const randomStuff = [
+  "credit card",
+  "screwdriver",
+  "receipt",
+  "gum",
+  "keys",
+  "used gum",
+  "plastic spoon",
+];
 
-console.log(findMyKeys(randomStuff))
-
+console.log(findMyKeys(randomStuff));
 
 const dogFactory = (name, breed, weight) => {
-    return {
-        _name: name,
-        _breed: breed,
-        _weight: weight,
-        get name() {
-            return this._name;
-        },
-        set name(newName) {
-            this._name = newName;
-        },
-        get breed() {
-            return this._breed;
-        },
-        set breed(newBreed) {
-            this._breed = newBreed;
-        },
-        get weight() {
-            return this._weight;
-        },
-        set weight(newWeight) {
-            this._weight = newWeight;
-        },
-        bark() {
-            return 'ruff! ruff!'
-        },
-        eatTooManyTreats() {
-            this._weight++
-        }
-    }
-}
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+    get name() {
+      return this._name;
+    },
+    set name(newName) {
+      this._name = newName;
+    },
+    get breed() {
+      return this._breed;
+    },
+    set breed(newBreed) {
+      this._breed = newBreed;
+    },
+    get weight() {
+      return this._weight;
+    },
+    set weight(newWeight) {
+      this._weight = newWeight;
+    },
+    bark() {
+      return "ruff! ruff!";
+    },
+    eatTooManyTreats() {
+      this._weight++;
+    },
+  };
+};
 
 /*
 // Solution to checkpoint 1:
@@ -236,4 +251,3 @@ const dogFactory = (name, breed, weight) => {
 }
 
 */
-
