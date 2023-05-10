26cb47e9cd18919b423443d7aa967637a498b89e
atsuki <csatsfapsfu2930@gmail.com>
2023-05-10 00:17:36 -0700

    learned basic id manupulation

---
+++ websiteJS/DOM/main.js
@@ -0,0 +1,11 @@
+document.body.innerHTML =
+  "<h1>This is now the heading of the body element</h1>";
+
+document.getElementById("first").innerHTML =
+  "<h1>Most popular Harry Potter characters</h1>";
+
+document.getElementById("fourth").innerHTML = "Professor Snape";
+
+document.getElementsByClassName("slytherin")[0].innerHTML = "Salazar Slytherin";
+
+document.getElementsByTagName("li")[0].innerHTML = "Dobby";
