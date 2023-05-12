07d8ee0def3500cc1502f0988ab6efa4bad97763
atsuki <csatsfapsfu2930@gmail.com>
2023-05-12 16:16:14 -0700

    added event type

--- websiteJS/DOM/event.js
+++ websiteJS/DOM/event.js
@@ -9,3 +9,25 @@ let sharephoto = function(event) {
 }
 
 share.addEventListener('click', sharephoto)
+
+
+//Event Type
+let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
+// This variable stores the "Pick a Color" button
+let button = document.getElementById('color-button');
+
+// This variable stores the "Mystery Color" button
+let mysteryButton = document.getElementById('next-button');
+
+// This random number function will create color codes for the randomColor variable
+function colorValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function colorChange(event){
+  button.style.backgroundColor = randomColor
+}
+
+button.addeventlistener('click', colorChange)
+
+mysteryButton.addEventListener('wheel', colorChange)
