8d27c8c1354dacb43c0a13a12f6ad10b9f0620a5
atsuki <csatsfapsfu2930@gmail.com>
2023-05-12 16:38:02 -0700

    implemented mouseevent

--- websiteJS/DOM/event.js
+++ websiteJS/DOM/event.js
@@ -31,3 +31,49 @@ function colorChange(event){
 button.addeventlistener('click', colorChange)
 
 mysteryButton.addEventListener('wheel', colorChange)
+
+
+// Mouse events
+
+// These variables store the boxes on the side
+let itemOne = document.getElementById('list-item-one');
+let itemTwo = document.getElementById('list-item-two');
+let itemThree = document.getElementById('list-item-three');
+let itemFour = document.getElementById('list-item-four');
+let itemFive = document.getElementById('list-item-five');
+let resetButton = document.getElementById('reset-button');
+
+// This function programs the "Reset" button to return the boxes to their default styles
+let reset = function() {
+  itemOne.style.width = ''
+  itemTwo.style.backgroundColor = ''
+  itemThree.innerHTML = 'The mouse must leave the box to change the text'
+  itemFive.style.display = "none"
+};
+resetButton.onclick = reset;
+
+// Write your code here
+
+function increaseWidth(){
+  itemOne.style.width = '400px' 
+}
+
+itemOne.addEventListener('mouseover', increaseWidth)
+
+function changeBackground(){
+  itemTwo.style.backgroundColor = 'blue'
+}
+
+itemTwo.addEventListener('mouseup',changeBackground)
+
+function changeText() {
+  itemThree.innerHTML = 'The mouse has left the element'
+}
+
+itemThree.addEventListener('mouseout', changeText)
+
+function showItem(){
+  itemFive.style.display = 'block'
+}
+
+itemFour.addEventListener('mousedown', showItem)
