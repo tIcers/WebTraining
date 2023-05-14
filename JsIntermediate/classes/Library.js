8cea949f521913b93d72ee081321647da9607447
atsuki <csatsfapsfu2930@gmail.com>
2023-05-14 15:51:32 -0700

    finished implementing all classes

--- JsIntermediate/classes/Library.js
+++ JsIntermediate/classes/Library.js
@@ -13,24 +13,86 @@ class Media {
   get getRatings() {
     return this._ratings;
   }
-  toggleCheckOutStatus(bool) {
-    if (bool === "true") {
-      setIsCheckedout(false);
+  set setIsCheckedout(bool) {
+    this._isCheckedout = bool;
+  }
+
+  toggleCheckOutStatus() {
+    if (this._isCheckedout === true) {
+      this.setIsCheckedout = false;
     } else {
-      setIsCheckedout(true);
+      this.setIsCheckedout = true;
     }
   }
   getAverageRating() {
     const sum = this._ratings.reduce((previous, current) => {
       return previous + current;
     }, 0);
-    return sum / this._ratins.length;
+    return sum / this._ratings.length;
   }
 
   addRating(newRating) {
     this._ratings.push(newRating);
   }
-  set setIsCheckedout(bool) {
-    this._isCheckedout = bool;
+}
+
+class Book extends Media {
+  constructor(title, isCheckedout, ratings, author, pages) {
+    super(title, isCheckedout, ratings);
+    (this._author = author), (this._pages = pages);
+  }
+  get getAuthor() {
+    return this._author;
+  }
+  get getPages() {
+    return this._pages;
   }
 }
+
+class Movie extends Media {
+  constructor(title, isCheckedout, ratings, director, runTime) {
+    super(title, isCheckedout, ratings);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get getDirector() {
+    return this._director;
+  }
+  get getRuntime() {
+    return this._runTime;
+  }
+}
+
+const historyOfEverything = new Book(
+  (title = "A Short History of Nearly Everything"),
+  (isCheckedout = false),
+  (ratings = []),
+  (author = "Bill Bryson"),
+  (pages = 544)
+);
+console.log(historyOfEverything);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(4);
+
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie(
+  (title = "speed"),
+  (isCheckedout = false),
+  (ratings = []),
+  (director = "Jan de Bont"),
+  (runTime = 116)
+);
+console.log(speed.toggleCheckOutStatus());
+console.log(speed.getIsCheckedout);
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+console.log(speed.getAverageRating());
