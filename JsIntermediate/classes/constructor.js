e22fc5a8157ef7c58027e32172b0882f58cce49c
atsuki <csatsfapsfu2930@gmail.com>
2023-05-14 10:37:00 -0700

    learned classes

--- JsIntermediate/classes/constructor.js
+++ JsIntermediate/classes/constructor.js
@@ -1,9 +1,26 @@
 class Surgeon {
   constructor(name, department) {
-    this.name = name;
-    this.department = department;
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get department() {
+    return this._department;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysoff) {
+    this._remainingVacationDays = -daysoff;
   }
 }
 const surgeonRomero = new Surgeon("Francisco Romero", "Cardiovascular");
 
 const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics");
+
+console.log(surgeonRomero.name);
+surgeonRomero.takeVacationDays(3);
+console.log(surgeonRomero.remainingVacationDays);
