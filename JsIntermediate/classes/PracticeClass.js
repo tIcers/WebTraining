f5723a835e79f7e6ed807f3bb70a794ab4b23489
atsuki <csatsfapsfu2930@gmail.com>
2023-05-20 19:36:41 -0700

    solved 2 questions

--- JsIntermediate/classes/PracticeClass.js
+++ JsIntermediate/classes/PracticeClass.js
@@ -12,3 +12,27 @@ class Network {
     }
   }
 }
+
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+
+  status() {
+    console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`);
+  }
+}
+const balloonAttack = (player1, player2) => {
+  for (let i = 1; i <= 10; i++) {
+    player2.balloonCount -= player1.hitsPerMinute;
+    player1.balloonCount -= player2.hitsPerMinute;
+    player1.status();
+    player2.status();
+  }
+  if (player1.balloonCount > player2.balloonCount) {
+    return player1.name;
+  } else if (player2.balloonCount > player1.balloonCount) return player2.name;
+  return "Tie";
+};
