de17c9ab246f9132db40d0c200d09e8d3d42e6ce
atsuki <csatsfapsfu2930@gmail.com>
2023-05-20 17:08:10 -0700

    imported module from another file

--- JsIntermediate/workAroundExplorer/main.js
+++ JsIntermediate/workAroundExplorer/main.js
@@ -1,8 +1,10 @@
 // TODO: Add your import statements here.
-
+import {getRoles, getCompanies from './salaryData.js'}
+import {getAverageSalaryByRole, getSalaryAtCompany, getIndustryAverageSalary, getAverageSalaryByCompany 
+from './workAroundModule.js'}
 // TODO: Get the companies and roles using the salaryData module.
-const companies = [];
-const roles = [];
+const companies = getCompanies();
+const roles = getRoles();
 
 // Create input buttons for every company and role represented in the data.
 renderInputButtons(companies, "company");
@@ -61,7 +63,46 @@ function updateResults() {
   const salary = 0;
   const industryAverageSalary = 0;
 
-  // Render them to the screen.
+  // Render them t// Add your imports here.
+  //
+  // // Replace the empty array with the appropriate imported function/value
+  // const getAverageSalaryByRole = role => {
+  //   const roleData = [];
+  //     const salariesOfRole = roleData.map(obj => obj.salary);
+  //       return calculateAverage(salariesOfRole);
+  //       }
+  //
+  //       // Replace the empty array with the appropriate imported function/value
+  //       const getAverageSalaryByCompany = company => {
+  //         const companyData = [];
+  //           const salariesAtCompany = companyData.map(obj => obj.salary);
+  //             return calculateAverage(salariesAtCompany);
+  //             }
+  //
+  //             // Replace the empty array with the appropriate imported function/value
+  //             const getSalaryAtCompany = (role, company) => {
+  //               const companyData = [];
+  //                 const roleAtCompany = companyData.find(obj => obj.role === role);
+  //                   return roleAtCompany.salary;
+  //                   }
+  //
+  //                   // Replace the empty array with the appropriate imported function/value
+  //                   const getIndustryAverageSalary = () => {
+  //                     const allSalaries = [].map(obj => obj.salary);
+  //                       return calculateAverage(allSalaries);
+  //                       }
+  //
+  //
+  //                       // Helper Function. Do not edit.
+  //                       // Note: This function does not need to be exported since it is only used by the functions contained within this module.
+  //                       function calculateAverage(arrayOfNumbers) {
+  //                         let total = 0;
+  //                           arrayOfNumbers.forEach(number => total += number);
+  //                             return (total / arrayOfNumbers.length).toFixed(2);
+  //                             }
+  //
+  //
+  //                             o the screen.lkj
   document.getElementById(
     "salarySelected"
   ).innerText = `The salary for ${role}s at ${company} is \$${salary}`;
