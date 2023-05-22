985ad1e0f1fe9d5b9accf9319f4a98a50f4e9704
atsuki <csatsfapsfu2930@gmail.com>
2023-05-22 13:06:28 -0700

    implemented setTimeOut fucntion for practice

--- AsyncReq/settimer.js
+++ AsyncReq/settimer.js
@@ -2,5 +2,11 @@ console.log("This is the first line of code in app.js.");
 // Keep the line above as the first line of code
 // Write your code here:
 
+function usingSTO() {
+  console.log("Hello, this is using STO");
+}
+
+setTimeout(usingSTO, 2400);
+
 // Keep the line below as the last line of code:
 console.log("This is the last line of code in app.js.");
