9d3673cd0b8dc550167688a9f43da9fda5223383
atsuki <csatsfapsfu2930@gmail.com>
2023-05-25 10:43:39 -0700

    implmented promise all

--- AsyncReq/promiseAll.js
+++ AsyncReq/promiseAll.js
@@ -11,4 +11,12 @@ const onReject = (rejectionReason) => {
   console.log(rejectionReason);
 };
 
-checkSunglasses = checkAvailability();
+const checkSunglasses = checkAvailability("sunglasses", "Favorite Supply Co.");
+
+const checkPants = checkAvailability("pants", "Favorite Supply Co.");
+
+const checkBags = checkAvailability("bags", "Favorite Supply Co.");
+
+const promiseArray = [checkSunglasses, checkPants, checkBags];
+
+Promise.all(promiseArray).then(onFulfill).catch(onReject);
