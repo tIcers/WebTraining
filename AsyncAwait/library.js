ef0b1f7325faef2523da6f343b5e5ef769f16cba
atsuki <csatsfapsfu2930@gmail.com>
2023-05-25 15:43:50 -0700

    added libary.js

--- AsyncAwait/library.js
+++ AsyncAwait/library.js
@@ -1,17 +1,39 @@
 /*
-This is the shopForBeans function. It uses a setTimeout() function to simulate a time-consuming asynchronous action. The function returns a promise with a resolved value of a string representing a type of bean. It settles on a random beanType from the beanTypes array using Math.random().
+This is the shopForBeans function from the last exercise
 */
 
 const shopForBeans = () => {
   return new Promise((resolve, reject) => {
     const beanTypes = ["kidney", "fava", "pinto", "black", "garbanzo"];
     setTimeout(() => {
-      let randomIndex = Math.floor(Math.random() * beanTypes.length);
+      let randomIndex = Math.floor(Math.random() * 5);
       let beanType = beanTypes[randomIndex];
-      console.log(`2. I bought ${beanType} beans because they were on sale.`);
+      console.log(`I bought ${beanType} beans because they were on sale.`);
       resolve(beanType);
     }, 1000);
   });
 };
 
-module.exports = shopForBeans;
+let soakTheBeans = (beanType) => {
+  return new Promise((resolve, reject) => {
+    console.log("Time to soak the beans.");
+    setTimeout(() => {
+      console.log(`... The ${beanType} beans are softened.`);
+      resolve(true);
+    }, 1000);
+  });
+};
+
+let cookTheBeans = (isSoftened) => {
+  return new Promise((resolve, reject) => {
+    console.log("Time to cook the beans.");
+    setTimeout(() => {
+      if (isSoftened) {
+        console.log("... The beans are cooked!");
+        resolve("\n\nDinner is served!");
+      }
+    }, 1000);
+  });
+};
+
+module.exports = { shopForBeans, soakTheBeans, cookTheBeans };
