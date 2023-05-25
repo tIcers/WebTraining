6632fd1d9357c0f733e49c937cc213035d22616f
atsuki <csatsfapsfu2930@gmail.com>
2023-05-25 14:51:32 -0700

    added basic async.js

---
+++ AsyncAwait/asyncApp.js
@@ -0,0 +1,16 @@
+const brainstormDinner = require("./asyncLibrary.js");
+
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+    console.log(`I'm going to make ${meal} for dinner.`);
+  });
+}
+
+// async/await version:
+async function announceDinner() {
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`);
+}
+
+announceDinner();
