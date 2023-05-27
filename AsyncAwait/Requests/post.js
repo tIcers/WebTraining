2df650bb963e0b99ff6bd38ab48102014d71d627
atsuki <csatsfapsfu2930@gmail.com>
2023-05-26 21:05:52 -0700

    added post request js

---
+++ AsyncAwait/Requests/post.js
@@ -0,0 +1,37 @@
+// Information to reach API
+const apiKey = "e39c21be9925499b9529ff86b862a7f2";
+const url = "https://api.rebrandly.com/v1/links";
+
+// Some page elements
+const inputField = document.querySelector("#input");
+const shortenButton = document.querySelector("#shorten");
+const responseField = document.querySelector("#responseField");
+
+// Asynchronous functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value.urlToShorten
+  const data = JSON.stringify({destination: urlToShorten})
+  fetch(url, {
+    method: 'POST',
+    headers :{
+    'Content-type' :'application/json',
+    'apikey': apiKey
+  }, 
+  body:data
+  ){
+ }
+}
+
+
+};
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while (responseField.firstChild) {
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+};
+
+shortenButton.addEventListener("click", displayShortUrl);
