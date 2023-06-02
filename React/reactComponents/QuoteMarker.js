638b765bf7024d31d5e81ef6ffaf96b22b4f355b
atsuki <csatsfapsfu2930@gmail.com>
2023-06-01 17:15:42 -0700

    implemented quote marker

---
+++ React/reactComponents/QuoteMarker.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function QuoteMaker() {
+  return (
+    <blockquote>
+      <p>
+        The world is full of objects, more or less interesting; I do not wish to
+        add any more.
+      </p>
+      <cite>
+        <a target="_blank" href="https://en.wikipedia.org/wiki/Douglas_Huebler">
+          Douglas Huebler
+        </a>
+      </cite>
+    </blockquote>
+  );
+}
+
+export default QuoteMaker;
