ee3650aaf042fcea217fb4dc89c9b9e4e5ae4d66
atsuki <csatsfapsfu2930@gmail.com>
2023-06-02 23:33:48 -0700

    done

---
+++ React/Props/PropsChildren.js
@@ -0,0 +1,18 @@
+import React from "react";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <List type="Living Musician">
+        <li>Sachiko M</li>
+        <li>Harvey Sid Fisher</li>
+      </List>
+      <List type="Living Cat Musician">
+        <li>Nora the Piano Cat</li>
+      </List>
+    </div>
+  );
+}
+
+export default App;
