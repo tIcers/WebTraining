9bc55318631048383a274ce77df4cf20abbe4473
atsuki <csatsfapsfu2930@gmail.com>
2023-06-05 21:18:19 -0700

    impmented app.js

--- React/Props/CodeyOverflow/App.js
+++ React/Props/CodeyOverflow/App.js
@@ -1,2 +1,18 @@
 import React from "react";
 import { comments } from "./commentData";
+import Header from "./Header.js";
+import Body from "./Body.js";
+import Card from "./Card.js";
+
+function App() {
+  return (
+    <div>
+      <h1>this is test mf</h1>
+      {comments.map((comment) => {
+        return <Card key={comment.id} commentObject={comment} />;
+      })}
+    </div>
+  );
+}
+
+export default App;
