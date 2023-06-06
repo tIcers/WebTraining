400499b2c5ed1191cf5ceb65a93a882b2ab52f9a
atsuki <csatsfapsfu2930@gmail.com>
2023-06-05 21:18:37 -0700

    implement card components

--- React/Props/CodeyOverflow/Card.js
+++ React/Props/CodeyOverflow/Card.js
@@ -1,1 +1,13 @@
 import React from "react";
+import Header from "./Header.js";
+import Body from "./Body.js";
+
+function Card(props) {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+
+export default Card
