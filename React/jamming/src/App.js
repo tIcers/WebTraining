00053746385fd560b271ce08727c8570ef685bad
atsuki <csatsfapsfu2930@gmail.com>
2023-06-09 15:41:28 -0700

    rendered search button and bar

--- React/jamming/src/App.js
+++ React/jamming/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
-
+import { SearchBar ,SearchButton} from './SearchBar';
 function App() {
   const headerStyle = {
     backgroundColor:'purple'
   }
+  // const appStyle = {
+  //   backgroundColor:
+  // }
   return (
     <div className="App" style={headerStyle}>
       <Header />
       <header className="App-header">
+        <div>
+          <SearchBar/>
+          <SearchButton/>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
