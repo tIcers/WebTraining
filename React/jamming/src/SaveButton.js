c6f3e4e90e11ffe20b2ae7cbd469bed25bf9933e
atsuki <csatsfapsfu2930@gmail.com>
2023-06-09 15:51:31 -0700

    created savebutton

---
+++ React/jamming/src/SaveButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SaveButton.css'
+
+function SaveButton(){
+  return (
+    <div className='save-button'>
+      <span type='submit'>Save To Spotify</span>
+    </div>
+  )
+}
+
+export default SaveButton
