11407c815df1a8a029a104c531bac289e8d7f2a0
atsuki <csatsfapsfu2930@gmail.com>
2023-06-11 15:22:45 -0700

    initila commmit for playlist

---
+++ React/jamming/src/Playlist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SaveButton from './SaveButton'
+
+
+function Playlist(){
+  return (
+    <div>
+      <h2>Playlist</h2>
+      <SaveButton />
+    </div>
+  )
+}
+
+export default Playlist
