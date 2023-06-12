3284ce9065bbd94fbe2cf68dd8138e818406c3f3
atsuki <csatsfapsfu2930@gmail.com>
2023-06-12 14:42:23 -0700

    implemented addOntrack function

--- React/jamming/src/Track.js
+++ React/jamming/src/Track.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Track.css'
-function Track({track}){
+function Track({track, onAddTrack}){
+  const handleAddTrack = () => {
+    onAddTrack(track)
+  }
   return (
    <div className='track-card'>
-      <h3>{track.name}</h3>
+      <h4>{track.name}</h4>
  <div className="track-details">
         <p>{track.artist}</p>
         <p>{track.album}</p>
     </div> 
+      <button onClick={handleAddTrack}>Add</button>
     </div>
   )
 }
