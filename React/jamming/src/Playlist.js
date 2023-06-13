3630bc51b3c0266dc1f77a953f9978636645cfd0
atsuki <csatsfapsfu2930@gmail.com>
2023-06-13 12:31:26 -0700

    handled rename playlist

--- React/jamming/src/Playlist.js
+++ React/jamming/src/Playlist.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React , {useState } from 'react'
 import PlaylistTrack from './PlaylistTrack'
+
 function Playlist({tracks, onRemoveTrack}){
-  return (
+
+  const [playlistName, setPlaylistName] = useState('My Playlist')
+
+  function handlePlaylistNameChange(event){
+    setPlaylistName(event.target.value)
+  }
+
+  function handlePlaylistNameSubmit(event){
+    event.preventDefault()
+  }
+
+   return (
     <div>
       <h2>Playlist</h2>
+      <form onSubmit={handlePlaylistNameSubmit}>
+        <input type="text" value={playlistName} onChange={handlePlaylistNameChange}/>
+        <button type='submit'>Update Playlist Name</button>
+      </form>
       {tracks.map((track)=> (
         <PlaylistTrack key={track.id} track={track} onRemoveTrack={ onRemoveTrack }/>
       ))}
