44ef0991ad480734cd8c75099193e3a6369d0c35
atsuki <csatsfapsfu2930@gmail.com>
2023-06-13 11:44:12 -0700

    implemented remove function

--- React/jamming/src/PlaylistTrack.js
+++ React/jamming/src/PlaylistTrack.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './PlaylistTrack.css'
-function PlaylistTrack({track}){
+function PlaylistTrack({track,onRemoveTrack}){
+  const handleRemoveTrack = () => {
+    onRemoveTrack(track)
+  }
+
   return (
     <div className='playlist-track'>
       <h3>{track.name}</h3>
       <div className='playlist-details'>
       <p>{track.artist} - {track.artist}</p>
+      </div>
+      <div>
+        <button onClick={handleRemoveTrack}>-</button>
       </div>
     </div>
   )
