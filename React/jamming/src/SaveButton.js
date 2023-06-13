d6ca4ce84ccd58bd75308b0fdeecc186f018ea9f
atsuki <csatsfapsfu2930@gmail.com>
2023-06-13 15:08:49 -0700

    implemented setplaylist to make clear playlist after save

--- React/jamming/src/SaveButton.js
+++ React/jamming/src/SaveButton.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './SaveButton.css'
 
-function SaveButton(){
+function SaveButton({playlist = [], setPlaylist}){
+  const handleSaveToSpotify = () => {
+    const trackURIs = playlist.map((track) => track.uri)
+    setPlaylist([])
+  }
+
+
   return (
     <div className='save-button'>
-      <span type='submit'>Save To Spotify</span>
+      <span onClick={handleSaveToSpotify} >Save to Spotify</span>
     </div>
   )
 }
