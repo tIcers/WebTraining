b8a6687af0e83e3d1747971eee8231fce3cc7e37
atsuki <csatsfapsfu2930@gmail.com>
2023-06-13 11:44:17 -0700

    added props

--- React/jamming/src/Playlist.js
+++ React/jamming/src/Playlist.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PlaylistTrack from './PlaylistTrack'
-function Playlist({tracks}){
+function Playlist({tracks, onRemoveTrack}){
   return (
     <div>
       <h2>Playlist</h2>
       {tracks.map((track)=> (
-        <PlaylistTrack key={track.id} track={track}/>
+        <PlaylistTrack key={track.id} track={track} onRemoveTrack={ onRemoveTrack }/>
       ))}
      </div>
   )
