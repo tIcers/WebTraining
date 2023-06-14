33031e196ea8ae6f86eeef4bf2208a265566805c
atsuki <csatsfapsfu2930@gmail.com>
2023-06-13 18:26:52 -0700

    set initial value

--- React/Hooks/ClolorPicker.js
+++ React/Hooks/ClolorPicker.js
@@ -1,26 +1,24 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
 
 export default function ColorPicker() {
- const [color, setColor ] = useState() 
+  const [color, setColor] = useState("Tomato");
 
  const divStyle = {backgroundColor: color};
 
   return (
     <div style={divStyle}>
-      <p>The color is {color}</p>
-      <button onClick={() => setColor('Aquamarine')}>
-        Aquamarine
-      </button>
-      <button onClick={() => setColor('BlueViolet')}>
-        BlueViolet
-      </button>
-      <button onClick={() => setColor('Chartreuse')}>
-        Chartreuse
-      </button>
-      <button onClick={() => setColor('CornflowerBlue')}>
-        CornflowerBlue
-      </button>
+      <p>Selected color: {color}</p>
+      {colorNames.map((colorName)=>(
+        <button 
+          onClick={() => setColor(colorName)} 
+          key={colorName}>
+       	     {colorName}
+      	</button>
+      ))}
     </div>
   );
 }
+
 
