b550c1e1fbe380dbb1ca5251a44c0a79596ae953
atsuki <csatsfapsfu2930@gmail.com>
2023-06-17 00:18:48 -0700

    import styole

---
+++ React/IntermediateReact/AttentionGrabber.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from 'AttentionGrabber.module.css'
+
+
+function AttentionGrabber() {
+  return (
+    <>
+      <h1 className={styles.h1}>Hi! Welcome to my portfolio.</h1>
+      <p>I'm a developer who likes designing user-friendly websites.</p>
+    </>
+  )
+}
+
+export default AttentionGrabber
