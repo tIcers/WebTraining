9b0e18b00cff5c3f53931c06ba335f36fe766e5b
atsuki <csatsfapsfu2930@gmail.com>
2023-06-17 22:56:55 -0700

    finished styling

--- React/IntermediateReact/styles/RockPaperScissors/Game.js
+++ React/IntermediateReact/styles/RockPaperScissors/Game.js
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
+import styles from './Game.module.css'
 
+const styles = {
+  fontSize:48,
+  margin:0
+}
 const CHOICES = [
   { name: "rock", emoji: "✊" },
   { name: "paper", emoji: "✋" },
   { name: "scissors", emoji: "✌️" },
 ];
+
+const choiceStyles = {
+  display:'flex',
+  alignItems:'center',
+  marginButtom:'40px',
+}
+
+const emojiStyles = {
+  size:'64px',
+  marginRight:'20px'
+}
+
+const nameStyles = {
+  margin:'0px',
+  fontSize:'24px',
+  color:'#ffff'
+}
+
+const resultStyles = {
+  marginTop:'40px',
+  fontSize:'48px',
+  color:'#ffff'
+}
 function Game() {
   const [playerChoice, setPlayerChoice] = useState(null);
   const [codeyChoice, setCodeyChoice] = useState(null);
@@ -34,11 +62,12 @@ function Game() {
   }
 
   return (
-    <div>
-      <h1>Rock Paper Scissors</h1>
-      <div>
+    <div className={styles.container}>
+      <h1 style={styles}>Rock Paper Scissors</h1>
+     <div>
         {CHOICES.map((choice) => (
           <button
+            className={styles.choices}
             key={choice.name}
             onClick={() => handlePlayerChoice(choice)}
             aria-label={choice.name}
@@ -49,15 +78,15 @@ function Game() {
       </div>
       {playerChoice && codeyChoice && (
         <div>
-          <div>
-            <span>{playerChoice.emoji}</span>
-            <p>You chose {playerChoice.name}</p>
+          <div style={choiceStyles}>
+            <span style={emojiStyles}>{playerChoice.emoji}</span>
+            <p style={nameStyles}>You chose {playerChoice.name}</p>
           </div>
-          <div>
-            <span>{codeyChoice.emoji}</span>
-            <p>The computer chose {codeyChoice.name}</p>
+          <div style={nameStyles}>
+            <span style={emojiStyles}>{codeyChoice.emoji}</span>
+            <p style={nameStyles}>The computer chose {codeyChoice.name}</p>
           </div>
-          <h2>{result}</h2>
+          <h2 style={resultStyles}>{result}</h2>
           <button onClick={resetGame}>Play again</button>
         </div>
       )}
