bba9720ee518bdbe465e87aa70fbf901b939016c
atsuki <csatsfapsfu2930@gmail.com>
2023-06-18 00:40:15 -0700

    finished react form

--- React/IntermediateReact/ReactForms/Input.js
+++ React/IntermediateReact/ReactForms/Input.js
@@ -13,12 +13,12 @@ function Input() {
         <p>Sign up for our newsletter to stay up-to-date on the latest products, receive exclusive discounts, and connect with other programmers who share your passion for all things tech.</p>
         <form>
           <label for="email">Email: </label>
-          <input id="email" type="text" onChange={handleUserInput}/>
+          <input id="email" type="text" onChange={handleUserInput} value={userInput}/>
         </form>
       </div>
       <div className={styles.inputDisplay}>
         <h2>Current User Input: </h2>
-        <h4></h4>
+        <h4>{userInput}</h4>
       </div>
     </>
     );
