662fc872d104ebd01053f19851f5017a0cace980
atsuki <csatsfapsfu2930@gmail.com>
2023-06-19 14:58:34 -0700

    redirect

--- React/Router/src/components/SignUp.js
+++ React/Router/src/components/SignUp.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import { signUp } from "../features/session/sessionSlice"
 import { useDispatch } from "react-redux";
-// import useNavigate
-
+import { useNavigate } from "react-router-dom";
 export default function SignUp () {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-   // Grab the navigate function
 
   const handleSubmit = e => {
     e.preventDefault();
     dispatch(signUp({username: username}));
-    // imperatively redirect the user to /profile
+    navigate('/profile')
   }
 
   return (
