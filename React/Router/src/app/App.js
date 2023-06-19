ee34693bf60be9f384fc0fb63e017288eaca3985
atsuki <csatsfapsfu2930@gmail.com>
2023-06-18 17:52:50 -0700

    made routers for each components

--- React/Router/src/app/App.js
+++ React/Router/src/app/App.js
@@ -10,15 +10,19 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route, RouterProvider} from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
+const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
+    <Route path="about" element={<About/>}/>
+    <Route path="sign-up" element={<SignUp/>}/>
+    <Route path="articles" element={<Articles/>}/>
+    <Route path="categories" element={<Categories/>}/>
+    <Route path="profile" element={<Profile/>}/>
   </Route>
+))
 
 function App() {
   return (
