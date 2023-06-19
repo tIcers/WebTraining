5682f7b3acf8ca74b040bfd083dd0ee94dd444b2
atsuki <csatsfapsfu2930@gmail.com>
2023-06-19 15:04:34 -0700

    implemented search params

--- React/Router/src/components/Articles.js
+++ React/Router/src/components/Articles.js
@@ -2,16 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 // Import Link and useSearchParams from React Router
 
 export default function Articles () {
   const articles = useSelector(selectArticles);
 
+  const [searchParams, setSearchParams] = useSearchParams()
+
   // Grab URLSearchParams object from useSearchParams hook
   
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
