00cf9b2682921173fb208431919eabe6ee0d26db
atsuki <csatsfapsfu2930@gmail.com>
2023-06-20 16:20:54 -0700

    deleted uncessary import

--- React/AdaptPet/src/components/root/index.js
+++ React/AdaptPet/src/components/root/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navigation from '../navigation';
 import { Outlet } from 'react-router-dom';
-// import outlet implement outlet
 
 const Root = () => {
     return (
