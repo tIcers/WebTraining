bd09aa8fae7296250f93e17f0075d5f818e3a9d5
atsuki <csatsfapsfu2930@gmail.com>
2023-06-20 16:20:32 -0700

    added route for type and id

--- React/AdaptPet/src/App.js
+++ React/AdaptPet/src/App.js
@@ -10,9 +10,10 @@ import { Route,  RouterProvider, createBrowserRouter,createRoutesFromElements } 
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element ={ <Root/> } >
     <Route index element={<HomePage/>}/>
-
-
-  </Route>))
+    <Route path=':type' element={<HomePage/>}/>
+    <Route path=':type/:id' element={<PetDetailsPage/>}/>
+  </Route>
+))
 
 function App() {
   return (
