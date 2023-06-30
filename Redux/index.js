44f7e030772cc430e6cd836e820b95d9fc04c46b
atsuki <csatsfapsfu2930@gmail.com>
2023-06-30 11:09:08 -0700

    added state and actions

---
+++ Redux/index.js
@@ -0,0 +1,15 @@
+const state = ['Take Five', 'Clair deLune', 'Respect']
+
+const addNewSong ={
+  type:'songs/addSong',
+  payload:'Halo',
+}
+
+const removeSong = {
+  type:'songs/removeSong',
+  payload:'Take Five'
+}
+
+const removeAll = {
+  type:'songs/removeAll'
+}
