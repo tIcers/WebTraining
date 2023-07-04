c3ee4e8c245cefb6b40ae734d028c953d2575f7a
atsuki <csatsfapsfu2930@gmail.com>
2023-07-04 14:54:15 -0700

    used provider from react-redux

---
+++ Redux/ReactRedux/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { loadData } from '../features/allRecipes/allRecipesSlice.js';
+import { AllRecipes } from '../features/allRecipes/AllRecipes.js';
+import { SearchTerm } from '../features/searchTerm/SearchTerm.js';
+import { FavoriteRecipes } from '../features/favoriteRecipes/FavoriteRecipes.js';
+
+export function App() {
+  return (
+    <main>
+      <section>
+        <SearchTerm />
+      </section>
+      <section>
+        <h2>Favorite Recipes</h2>
+        <FavoriteRecipes />
+      </section>
+      <hr />
+      <section>
+        <h2>All Recipes</h2>
+        <AllRecipes />
+      </section>
+    </main>
+  )
+}
