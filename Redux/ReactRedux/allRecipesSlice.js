060f6aea65a04b76f8107a08a982685a650d899e
atsuki <csatsfapsfu2930@gmail.com>
2023-07-04 15:35:06 -0700

    used selector

---
+++ Redux/ReactRedux/allRecipesSlice.js
@@ -0,0 +1,53 @@
+import allRecipesData from '../../../data.js'
+import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
+
+export const loadData = () => {
+  return {
+    type: 'allRecipes/loadData',
+    payload: allRecipesData
+  }
+}
+
+const initialState = [];
+export const allRecipesReducer = (allRecipes = initialState, action) => {
+  switch (action.type) {
+    case 'allRecipes/loadData':
+      return action.payload;
+    case 'favoriteRecipes/addRecipe':
+      return allRecipes.filter(recipe => recipe.id !== action.payload.id);
+    case 'favoriteRecipes/removeRecipe':
+      return [...allRecipes, action.payload]
+    default:
+      return allRecipes;
+  }
+}
+
+export const selectAllRecipes = state => state.allRecipes
+
+export const selectFilteredAllRecipes = state => {
+  const allRecipes = selectAllRecipes(state)
+  const searchTerm = selectSearchTerm(state)
+  return allRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  
+}
+
+
+// This code is for testing the selectors only.
+const testState = {
+  allRecipes: allRecipesData,
+  searchTerm: 'ch'
+}
+
+const testSelectAllRecipes = () => {
+  console.log('All Recipes')
+  console.log(selectAllRecipes(testState));
+}
+
+const testSelectFilteredAllRecipes = () => {
+  console.log('\nRecipes filtered by searchTerm')
+  console.log(selectFilteredAllRecipes(testState));
+}
+
+// Uncomment these to test each selector.
+// testSelectAllRecipes();
+// testSelectFilteredAllRecipes(); 
