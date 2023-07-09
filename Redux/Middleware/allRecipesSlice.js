4cce5386bb6e7279882aa8c93285fea60784f810
atsuki <csatsfapsfu2930@gmail.com>
2023-07-08 22:51:21 -0700

    refactored with createAsyncThunk

--- Redux/Middleware/allRecipesSlice.js
+++ Redux/Middleware/allRecipesSlice.js
@@ -1,13 +1,15 @@
 import { fetchRecipes } from '../../app/api'
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
-const loadRecipes = () => {
-  return async(dispatch) => {
-    const recipes = await fetchRecipes()
-    dispatch({type:'allRecipes/addRecipes', payload:recipes})
+const loadRecipes = createAsyncThunk(
+  'allRecipes/loadRecipes',
+  async(arg, thunkAPI) => {
+    const data = await fetchRecipes()
+    const json = await data.json()
+    return json 
   }
-}
+)
 
 export const allRecipesSlice = createSlice({
   name: "allRecipes",
