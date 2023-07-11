a9902d49a479b65f35c7a8d8b281c952782726bd
atsuki <csatsfapsfu2930@gmail.com>
2023-07-10 18:16:25 -0700

    added os module

--- Back-End/IntroNode/app.js
+++
@@ -1,8 +0,0 @@
-let noun1 = 'affinity';
-let adjective = 'silly';
-let noun2 = 'john';
-let verb = 'play';
-let noun3 = 'house';
-
-
-console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
--- Back-End/IntroNode/consoleModule.js
+++
@@ -1,7 +0,0 @@
-const petsArray = ['dog', 'cat', 'bird', 'monkey'];
-
-console.log(petsArray)
-console.table(petsArray)
-console.assert(petsArray.length > 5)
-
-
