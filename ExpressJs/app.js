8962d334bcf18dbd7384c29b24dac175cef2afe5
atsuki <csatsfapsfu2930@gmail.com>
2023-08-22 12:37:35 -0700

    setting status code

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -14,6 +14,15 @@ app.get('/expressions', (req, res, next) => {
   console.log(req);
 });
 
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions)
+  if (foundExpression){
+    res.send(foundExpression)
+  }else{
+    res.status(404).send('Expressions are not found')
+  }
+}) 
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
