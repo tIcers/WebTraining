d0e7b35fa838a08b2f200b643bf69387efe8dbf5
atsuki <csatsfapsfu2930@gmail.com>
2023-08-23 11:18:42 -0700

    refactored codes using route

--- ExpressJs/Routers/app.js
+++ ExpressJs/Routers/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const expressionsRouter = require('./expressions.js')
 
 const { getElementById, getIndexById, updateElement,
   seedElements, createElement } = require('./utils');
@@ -9,55 +8,15 @@ const PORT = process.env.PORT || 4001;
 // Use static server to serve the Express Yourself Website
 app.use(express.static('public'));
 
-const animals = [];
+let animals = [];
 seedElements(animals, 'animals');
+
+const expressionsRouter = require('./expressions.js');
 
 app.use('/expressions', expressionsRouter);
 
-
-
 // Get a single expression
-app.get('/expressions/:id', (req, res, next) => {
-  const foundExpression = getElementById(req.params.id, expressions);
-  if (foundExpression) {
-    res.send(foundExpression);
-  } else {
-    res.status(404).send();
-  }
-});
 
-// Update an expression
-app.put('/expressions/:id', (req, res, next) => {
-  const expressionIndex = getIndexById(req.params.id, expressions);
-  if (expressionIndex !== -1) {
-    updateElement(req.params.id, req.query, expressions);
-    res.send(expressions[expressionIndex]);
-  } else {
-    res.status(404).send();
-  }
-});
-
-// Create an expression
-app.post('/expressions', (req, res, next) => {
-  const receivedExpression = createElement('expressions', req.query);
-  if (receivedExpression) {
-    expressions.push(receivedExpression);
-    res.status(201).send(receivedExpression);
-  } else {
-    res.status(400).send();
-  }
-});
-
-// Delete an expression
-app.delete('/expressions/:id', (req, res, next) => {
-  const expressionIndex = getIndexById(req.params.id, expressions);
-  if (expressionIndex !== -1) {
-    expressions.splice(expressionIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
 
 // Get all animals
 app.get('/animals', (req, res, next) => {
--- ExpressJs/Routers/expressions.js
+++ ExpressJs/Routers/expressions.js
@@ -1,13 +1,56 @@
-
 const express = require('express');
-const { seedElements } = require('./utils');
+const { seedElements, getElementById, createElement, updateElement, getIndexById } = require('./utils');
 
-const expressionsRouter = express()
-
-const expressions = [];
+let expressions = [];
 seedElements(expressions, 'expressions');
 
+const expressionsRouter = express.Router();
+
+module.exports = expressionsRouter;
+
+// Get all expressions
 expressionsRouter.get('/', (req, res, next) => {
   res.send(expressions);
 });
-module.exports = expressionsRouter
+
+expressionsRouter.get('/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Update an expression
+expressionsRouter.put('/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Create an expression
+expressionsRouter.post('/', (req, res, next) => {
+  const receivedExpression = createElement('expressions', req.query);
+  if (receivedExpression) {
+    expressions.push(receivedExpression);
+    res.status(201).send(receivedExpression);
+  } else {
+    res.status(400).send();
+  }
+});
+
+// Delete an expression
+expressionsRouter.delete('/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    expressions.splice(expressionIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
