febf9e1491b78225be9d3b1e65280bc2f5817ca6
atsuki <csatsfapsfu2930@gmail.com>
2023-08-23 11:27:18 -0700

    refacred codes and files

--- ExpressJs/Routers/app.js
+++ ExpressJs/Routers/app.js
@@ -8,63 +8,12 @@ const PORT = process.env.PORT || 4001;
 // Use static server to serve the Express Yourself Website
 app.use(express.static('public'));
 
-let animals = [];
-seedElements(animals, 'animals');
+const animalsRouter = require('./animals.js');
+app.use('/animals', animalsRouter);
 
+// Import and mount the expressionsRouter
 const expressionsRouter = require('./expressions.js');
-
 app.use('/expressions', expressionsRouter);
-
-// Get a single expression
-
-
-// Get all animals
-app.get('/animals', (req, res, next) => {
-  res.send(animals);
-});
-
-// Get a single animal
-app.get('/animals/:id', (req, res, next) => {
-  const animal = getElementById(req.params.id, animals);
-  if (animal) {
-    res.send(animal);
-  } else {
-    res.status(404).send();
-  }
-});
-
-// Create an animal
-app.post('/animals', (req, res, next) => {
-  const receivedAnimal = createElement('animals', req.query);
-  if (receivedAnimal) {
-    animals.push(receivedAnimal);
-    res.status(201).send(receivedAnimal);
-  } else {
-    res.status(400).send();
-  }
-});
-
-// Update an animal
-app.put('/animals/:id', (req, res, next) => {
-  const animalIndex = getIndexById(req.params.id, animals);
-  if (animalIndex !== -1) {
-    updateElement(req.params.id, req.query, animals);
-    res.send(animals[animalIndex]);
-  } else {
-    res.status(404).send();
-  }
-});
-
-// Delete a single animal
-app.delete('/animals/:id', (req, res, next) => {
-  const animalIndex = getIndexById(req.params.id, animals);
-  if (animalIndex !== -1) {
-    animals.splice(animalIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
