b4caa1ed7333f7f729887919225df673877afcda
atsuki <csatsfapsfu2930@gmail.com>
2023-08-24 08:21:12 -0700

    route for random quote

--- quote-api/server.js
+++ quote-api/server.js
@@ -8,6 +8,16 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/quotes/random', (req, res, next) => {
+  const chosenQuote = getRandomElement(quotes)
+  res.send(chosenQuote['quote'])
+})
+
+app.get('/quotes', (req, res, next) => {
+
+})
+
+
 app.listen(PORT, (req, res, next )=> {
   console.log(`${PORT} is listening..`)
 })
