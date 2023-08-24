1415a40a4c939fbac6483ff36dff91fb16aa02ac
atsuki <csatsfapsfu2930@gmail.com>
2023-08-24 07:38:33 -0700

    setup starter code

---
+++ ExpressJs/RC/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+const battlefields = {
+  fortSumter: {
+    state: 'SC',
+  },
+  manassas: {
+    state: 'VA',
+  },
+  gettysburg: {
+    state: 'PA',
+  },
+  antietam: {
+    state: 'MD',
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/battlefields/:name', (req, res, next) => {
+  const battlefieldName = req.params.name;
+  const battlefield = battlefields[battlefieldName]
+  
+
+  if(battlefield){
+    res.send(battlefield)
+  }else{
+    res.status(404).send()
+  }
+});
---
+++ quote-api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+
+app.listen(PORT, (req, res, next )=> {
+  console.log(`${PORT} is listening..`)
+})
+
