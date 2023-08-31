28a0df402f69cc69dd61a6b85507e8374b084e8e
atsuki <csatsfapsfu2930@gmail.com>
2023-08-30 18:49:02 -0700

    Authentication and Sessions: Cookies

--- ExpressJs/SessionAuth/Session/app.js
+++ ExpressJs/SessionAuth/Session/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
-
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 4001;
+const store = new session.MemoryStore();
 
-const store = new session.MemoryStore()
 app.use(
   session({
-    secret:'sdfe293rGdf',
-    resave:false,
-    saveUninitialized:false,
+    secret: "D53gxl41G",
+    cookie:{maxAge:172800000, secure:true, sameSite:'none'},
+    resave: false,
+    saveUninitialized: false,
     store
   })
 );
