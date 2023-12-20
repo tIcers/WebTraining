5e26aac611f3e517294868ee1a0c036270a10314
atsuki <csatsfapsfu2930@gmail.com>
2023-12-19 23:02:50 -0800

    implemented passport localstrategy

---
+++ UserAuth/passport.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    db.users.findByUsername(username, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (user.password != password) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  }),
+);
+app.get("/", (req, res) => {
+  res.send("Hello from the homepage!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
