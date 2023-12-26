8b410e751943cdc1f1e98d260df97ff3485979d5
atsuki <csatsfapsfu2930@gmail.com>
2023-12-26 00:47:48 -0800

    implemented bcrypt with salt

---
+++ ExpressJs/Bcrypt/app.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcrypt");
+
+const passwordHash = async (password, saltRounds) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+};
+
+const comparePassword = async (password, hash) => {
+  try {
+    const matchFound = await bcrypt.compare(password, hash);
+    return matchFound;
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+};
