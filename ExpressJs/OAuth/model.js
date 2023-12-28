4ce24a24c9a4bb05455696e22ac4fd3ada7ffcab
atsuki <csatsfapsfu2930@gmail.com>
2023-12-27 21:42:47 -0800

    saveToken

--- ExpressJs/OAuth/model.js
+++ ExpressJs/OAuth/model.js
@@ -1,12 +1,23 @@
-let db = require('./db.js')
+let db = require("./db.js");
 
 const getClient = (clientId, clientSecret) => {
-  let confidentialClients = db.confidentialClients.filter((client) -> {
-    return client.clientId === client.clientId && client.clientSecret
-  })
-  return confidentialClients[0]
-}
+  let confidentialClients = db.confidentialClients.filter((client) => {
+    return client.clientId === clientId && client.clientSecret === clientSecret;
+  });
+  return confidentialClients[0];
+};
 
+const saveToken = (token, client, user) => {
+  token.client = {
+    id: client.clientId,
+  };
+  token.user = {
+    username: user.username,
+  };
+  db.tokens.push(token);
+  return token;
+};
 module.exports = {
-  getClient:getClient
-}
+  getClient: getClient,
+  saveToken: saveToken,
+};
