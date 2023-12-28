50eb398a32ec75da4df797f25a3a6115926512fe
atsuki <csatsfapsfu2930@gmail.com>
2023-12-28 01:07:12 -0800

    Obtaining Token Handler

--- ExpressJs/OAuth/app.js
+++ ExpressJs/OAuth/app.js
@@ -3,12 +3,32 @@ const path = require("path");
 const OAuth2Server = require("oauth2-server");
 
 const app = express();
-const oauth = new OAuth2Server();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = 4001;
+
+const oauth = new OAuth2Server({
+  model: require("./model"),
+  allowBearerTokensInQueryString: true,
+});
+
+const obtainToken = (req, res) => {
+  let request = new OAuth2Server.Request(req);
+  let response = new OAuth2Server.Response(res);
+  return oauth
+    .token(request, response)
+    .then((token) => {
+      res.json(token);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+app.all("/auth", obtainToken) => {
+})
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/home.html"));
